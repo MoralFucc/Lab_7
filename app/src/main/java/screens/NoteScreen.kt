0e587ffb9d0c4db3242c1527c814f5e179e03993
Artem Saluki <dnclahkam@gmail.com>
2023-05-28 23:39:09 +0300

    AppDrawer

---
+++ app/src/main/java/screens/NoteScreen.kt
@@ -0,0 +1,12 @@
+package screens
+
+import androidx.compose.runtime.Composable
+import com.topic2.android.notes.viewmodel.MainViewModel
+
+@Composable
+fun NotesScreen(
+    viewModel: MainViewModel
+){
+
+
+}
