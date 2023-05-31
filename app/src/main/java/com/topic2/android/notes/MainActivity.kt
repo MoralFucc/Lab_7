93fc832d96283a7e85585d9236d3fdce3ca50c62
Zver <dima.852.dma@gmail.com>
2023-05-31 23:38:19 +0300

    change MainActivity

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.topic2.android.notes
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.activity.compose.setContent
-import com.topic2.android.notes.viewmodel.MainViewModel
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import com.topic2.android.notes.routing.NotesRouter
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.theme.NotesTheme
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
+import screens.MainViewModel
+import screens.NotesScreen
+import screens.TrashScreen
+import ui.components.SaveNoteScreen
+
+
 
 /**
  * Main activity приложения.
  */
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
   private val viewModel: MainViewModel by viewModels(factoryProducer = {
     MainViewModelFactory(
       this,
@@ -19,11 +30,26 @@ class MainActivity : AppCompatActivity() {
     )
   })
 
+  @ExperimentalMaterialApi
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
-
+      NotesTheme {
+        MainActivityScreen(viewModel = viewModel)
+      }
     }
+  }
+}
+@Composable
+@ExperimentalMaterialApi
+private fun MainActivityScreen(viewModel: MainViewModel){
+  Surface {
+    when (NotesRouter.currentScreen){
+      is Screen.Notes -> NotesScreen(viewModel)
+      is Screen.SaveNote -> SaveNoteScreen(viewModel)
+      is Screen.Trash -> TrashScreen(viewModel)
+    }
+
   }
 }
